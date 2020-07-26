f36206efdbc74945c6c83bc97aa314c59435f7a1
Iván Alonso <ivan@neverbot.com>
2020-07-26 12:20:59 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,6 +5,7 @@ SSHPATH="$HOME/.ssh"
 mkdir -p "$SSHPATH"
 echo "$DEPLOY_KEY" > "$SSHPATH/key"
 chmod 600 "$SSHPATH/key"
-SERVER_DEPLOY_STRING="$USERNAME@$SERVER_IP:$SERVER_DESTINATION"
-# sync it up"
-sh -c "rsync $ARGS -e 'ssh -i $SSHPATH/key -o StrictHostKeyChecking=no -p $SERVER_PORT' $GITHUB_WORKSPACE/$FOLDER $SERVER_DEPLOY_STRING"
+DEPLOY_STRING="$REMOTE_USER@$REMOTE_SERVER:$REMOTE_PATH"
+# sync it up
+sh -c "rsync -ahz --force --delete --progress -e 'ssh -p22 -i $SSHPATH/key -o StrictHostKeyChecking=no -p $REMOTE_PORT' $GITHUB_WORKSPACE/$FOLDER $DEPLOY_STRING"
+#rsync -ahz --force --delete --progress --exclude-from=$EXCLUDE_FILE -e "ssh -p22 -i"$KEY_FILE $LOCAL_PATH $REMOTE_USER@$SERVER:$REMOTE_PATH
