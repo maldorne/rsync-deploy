19b7a34903eb6a87a961622913bf57572bf6dce8
neverbot <ivan@neverbot.com>
2020-07-26 17:09:43 +0200

    feat: use exclude list

--- script.sh
+++ script.sh
@@ -3,9 +3,14 @@ set -eu
 
 SSHPATH="$HOME/.ssh"
 mkdir -p "$SSHPATH"
-echo "$DEPLOY_KEY" > "$SSHPATH/key"
+echo "$DEPLOY_KEY" >> "$SSHPATH/key"
 chmod 600 "$SSHPATH/key"
+
+# file with a list of files/folders which are NOT to be rsync'd
+EXCLUDE_FILE="$HOME/exclude.txt"
+cat "$EXCLUDE_FILES" >> "$HOME/exclude.txt"
+
 DEPLOY_STRING="$REMOTE_USER@$REMOTE_SERVER:$REMOTE_PATH"
+
 # sync it up
-sh -c "rsync -ahz --force --delete --progress -e 'ssh -p22 -i $SSHPATH/key -o StrictHostKeyChecking=no -p $REMOTE_PORT' $GITHUB_WORKSPACE/$FOLDER $DEPLOY_STRING"
-#rsync -ahz --force --delete --progress --exclude-from=$EXCLUDE_FILE -e "ssh -p22 -i"$KEY_FILE $LOCAL_PATH $REMOTE_USER@$SERVER:$REMOTE_PATH
+sh -c "rsync -ahz --force --delete --progress --exclude-from=$EXCLUDE_FILE -e 'ssh -p22 -i $SSHPATH/key -o StrictHostKeyChecking=no -p $REMOTE_PORT' $GITHUB_WORKSPACE/$FOLDER $DEPLOY_STRING"
